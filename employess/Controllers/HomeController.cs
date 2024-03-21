ea00f70a9901f508b1261c6a50123c1f5c8a5cb8
bbjelica <bbjelica@propeler.io>
2024-03-21 15:31:29 +0100

    postavke

---
+++ employess/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using employess.Models;
+
+namespace employess.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly string endpoint = "https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code={key}";
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<string> GetAllEmployees()
+    {
+        string responseBody = "";
+
+        using(HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(endpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                responseBody = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                responseBody = "Greska pri preuzimanju podataka. Status code: " + response.StatusCode;
+            }
+        }
+
+        return responseBody;
+    }
+
+
+
+}
+
